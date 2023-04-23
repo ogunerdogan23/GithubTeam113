959ca05467133ebecc57b2ecb33f09f6758f1d29
Ogün Erdogan <ogun.erdogan23@gmail.com>
2023-04-23 20:41:29 +0200

    ilkRepository güncelleme1

--- src/Github_ders1.java
+++ src/Github_ders1.java
@@ -1,5 +1,7 @@
 public class Github_ders1 {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("=============");
+        System.out.println("Hello Gtihub Güncelleme 1");
     }
 }
